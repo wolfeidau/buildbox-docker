4e036adac80258f75772d4afaeed25d13bc0b633
Keith Pitt <me@keithpitt.com>
2014-02-24 22:51:56 +1100

    Log out the docker command to run.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "os"
   "fmt"
+  "log"
   "github.com/codegangsta/cli"
   "github.com/buildboxhq/agent-go/buildbox"
 )
@@ -19,7 +20,7 @@ func main() {
 
   app := cli.NewApp()
   app.Name = "buildbox-docker"
-  app.Version = "0.1"
+  app.Version = "0.1.alpha"
 
   // Define the actions for our CLI
   app.Flags = []cli.Flag {
@@ -28,18 +29,31 @@ func main() {
     cli.BoolFlag{"debug", "Enable debug mode."},
   }
 
-  // Default the default action
+  // Setup the main action for out application
   app.Action = func(c *cli.Context) {
     if c.String("access-token") == "" {
       fmt.Printf("buildbox-docker: missing access token\nSee 'buildbox-docker --help'\n")
       os.Exit(1)
     }
 
+    // Setup the HTTP client
     var client buildbox.Client;
     client.AgentAccessToken = c.String("access-token")
     client.URL = c.String("url")
     client.Debug = c.Bool("debug")
+
+    // Find out the image of the docker container to user
+    image := "d7694418f082"
+
+    // The ID of the job
+    job := "1234"
+
+    command := fmt.Sprintf("docker run %s /bin/bash --login -c 'buildbox-agent run %s --access-token %s --url %s'", image, job, client.AgentAccessToken, client.URL)
+
+    log.Printf(command)
+
   }
 
+  // Run our application
   app.Run(os.Args)
 }
