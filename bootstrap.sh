f89fdb14685977c1c91752c7a4d167fe63391e2a
Keith Pitt <me@keithpitt.com>
2014-03-15 17:30:30 +0800

    Updated the bootstrap.sh for allowing artifact uploading.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,69 @@
 #!/bin/bash
-set -e
-set -x
 
-echo '--- environmnet'
-env | grep BUILDBOX
+BUILDBOX_PROMPT="\033[90m$\033[0m"
 
-echo '--- starting services'
+function buildbox-exit-if-failed {
+  if [ $1 -ne 0 ]
+  then
+    exit $1
+  fi
+}
+
+function buildbox-run {
+  echo -e "$BUILDBOX_PROMPT $1"
+  eval $1
+  buildbox-exit-if-failed $?
+}
+
+echo '--- setting up environment'
+
+# Boot up stuff that isn't by default started in the container
 sudo /etc/init.d/postgresql start
 sudo /etc/init.d/redis start
 
-echo '--- setup ssh'
+# Setup the SSH keys for this agent
 echo "$BUILDBOX_AGENT_SSH_PRIVATE_KEY" >> ~/.ssh/id_rsa
 echo "$BUILDBOX_AGENT_SSH_PUBLIC_KEY" >> ~/.ssh/id_rsa.pub
 chmod 0600 ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa.pub
 
-echo '--- setting up repo'
+# This will return the location of this file. We assume that the buildbox-artifact
+# tool is in the same folder. You can of course customize the locations
+# and edit this file.
+BUILDBOX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Create the build directory
-BUILD_DIR="tmp/$BUILDBOX_AGENT_NAME/$BUILDBOX_PROJECT_SLUG"
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+BUILD_DIR="$BUILDBOX_AGENT_NAME/$BUILDBOX_PROJECT_SLUG"
+
+buildbox-run "mkdir -p $BUILD_DIR"
+buildbox-run "cd $BUILD_DIR"
 
 # Do we need to do a git checkout?
 if [ ! -d ".git" ]; then
-  git clone "$BUILDBOX_REPO" . -qv --depth 20
+  buildbox-run "git clone "$BUILDBOX_REPO" . -qv --depth 20"
 fi
 
-git clean -fdq
-git fetch -q
-git reset --hard origin/master
-git checkout -qf "$BUILDBOX_COMMIT"
+echo '--- preparing git'
 
-echo "--- running $BUILDBOX_SCRIPT_PATH"
+buildbox-run "git clean -fdq"
+buildbox-run "git fetch -q"
+buildbox-run "git reset --hard origin/master"
+buildbox-run "git checkout -qf \"$BUILDBOX_COMMIT\""
+
+echo "--- running build script"
+echo -e "$BUILDBOX_PROMPT ./$BUILDBOX_SCRIPT_PATH"
+
 ."/$BUILDBOX_SCRIPT_PATH"
+EXIT_STATUS=$?
+
+if [ "$BUILDBOX_ARTIFACT_PATHS" != "" ]
+then
+  echo "--- uploading artifacts"
+  echo -e "$BUILDBOX_PROMPT buildbox-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\""
+
+  $BUILDBOX_DIR/buildbox-artifact upload "$BUILDBOX_ARTIFACT_PATHS" --url $BUILDBOX_AGENT_API_URL
+  buildbox-exit-if-failed $?
+fi
+
+
+exit $EXIT_STATUS
