e6b23294e087e81c37d816558dc7f92c13f6919e
Keith Pitt <me@keithpitt.com>
2014-03-16 23:20:02 +0800

    Updated to the latest version of bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,22 +15,17 @@ function buildbox-run {
   buildbox-exit-if-failed $?
 }
 
-echo '--- setting up environment'
+echo '--- setup environment'
 
 # Boot up stuff that isn't by default started in the container
-sudo /etc/init.d/postgresql start
-sudo /etc/init.d/redis start
+buildbox-run "sudo /etc/init.d/postgresql start"
+buildbox-run "sudo /etc/init.d/redis start"
 
 # Setup the SSH keys for this agent
 echo "$BUILDBOX_AGENT_SSH_PRIVATE_KEY" >> ~/.ssh/id_rsa
 echo "$BUILDBOX_AGENT_SSH_PUBLIC_KEY" >> ~/.ssh/id_rsa.pub
 chmod 0600 ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa.pub
-
-# This will return the location of this file. We assume that the buildbox-artifact
-# tool is in the same folder. You can of course customize the locations
-# and edit this file.
-BUILDBOX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create the build directory
 BUILD_DIR="$BUILDBOX_AGENT_NAME/$BUILDBOX_PROJECT_SLUG"
@@ -39,31 +34,43 @@ buildbox-run "mkdir -p $BUILD_DIR"
 buildbox-run "cd $BUILD_DIR"
 
 # Do we need to do a git checkout?
-if [ ! -d ".git" ]; then
-  buildbox-run "git clone "$BUILDBOX_REPO" . -qv --depth 20"
+if [ ! -d ".git" ]
+then
+  buildbox-run "git clone "$BUILDBOX_REPO" . -qv"
 fi
-
-echo '--- preparing git'
 
 buildbox-run "git clean -fdq"
 buildbox-run "git fetch -q"
 buildbox-run "git reset --hard origin/master"
 buildbox-run "git checkout -qf \"$BUILDBOX_COMMIT\""
 
-echo "--- running build script"
-echo -e "$BUILDBOX_PROMPT ./$BUILDBOX_SCRIPT_PATH"
+echo "--- running $BUILDBOX_SCRIPT_PATH"
 
-."/$BUILDBOX_SCRIPT_PATH"
-EXIT_STATUS=$?
+if [ "$BUILDBOX_SCRIPT_PATH" == "" ]
+then
+  echo "ERROR: No script path has been set for this project. Please go to \"Project Settings\" and add the path to your build script"
+  exit 1
+else
+  ."/$BUILDBOX_SCRIPT_PATH"
+  EXIT_STATUS=$?
+fi
 
 if [ "$BUILDBOX_ARTIFACT_PATHS" != "" ]
 then
-  echo "--- uploading artifacts"
-  echo -e "$BUILDBOX_PROMPT buildbox-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\""
+  # Test to make sure buildbox-artifact is in the $PATH
+  command -v buildbox-artifact >/dev/null 2>&1 || { echo >&2 "ERROR: buildbox-artifact could not be found in \$PATH"; exit 1; }
 
-  $BUILDBOX_DIR/buildbox-artifact upload "$BUILDBOX_ARTIFACT_PATHS" --url $BUILDBOX_AGENT_API_URL
+  # If you want to upload artifacts to your own server, uncomment the lines below
+  # and replace the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY with keys to your
+  # own bucket.
+  # export AWS_SECRET_ACCESS_KEY=yyy
+  # export AWS_ACCESS_KEY_ID=xxx
+  # buildbox-artifact upload "$BUILDBOX_ARTIFACT_PATHS" s3://bucket-name/foo/bar --url $BUILDBOX_AGENT_API_URL
+
+  # By default we silence the buildbox-artifact build output. However, if you'd like to see
+  # it in your logs, remove the: > /dev/null 2>&1 from the end of the line.
+  buildbox-artifact upload "$BUILDBOX_ARTIFACT_PATHS" --url $BUILDBOX_AGENT_API_URL > /dev/null 2>&1
   buildbox-exit-if-failed $?
 fi
-
 
 exit $EXIT_STATUS
