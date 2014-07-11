660934714ea02cf5c5bd53d5c8b1c2c302b8ce6e
Keith Pitt <me@keithpitt.com>
2014-07-11 10:41:10 +0800

    Rearranged some scripts.

--- docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-docker build --tag "buildboxhq/base" .
--- login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-export DOCKER_HOST=tcp://0.0.0.0:4243
-docker run -rm -i -t buildboxhq/base /bin/bash --login
